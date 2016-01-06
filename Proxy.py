d1b02d722baff5a951b3cceec9ee605987a3a775
Mickey Liu <junjiliu@ebay.com>
2016-01-06 16:26:00 +0800

    Sync

--- Proxy.py
+++ Proxy.py
@@ -3,13 +3,14 @@
 
 import crawl
 import time
-import threading
 import urllib2
 from bs4 import BeautifulSoup
 
 class crawl_Proxy(crawl.Crawl):
     def __init__(self):
         self.domestic_proxy_url = "http://www.haodailiip.com/guonei/"
+        self.timeout = 5
+        self.testUrl = "http://www.baidu.com/"
 
     def crawl_proxy(self,max_pages=50000):
         pages = 1
@@ -41,23 +42,16 @@ class crawl_Proxy(crawl.Crawl):
             time.sleep(5)
         return whole_list
 
-class ProxyCheck(threading.Thread):
-    def __init__(self,proxyList):
-        threading.Thread.__init__(self)
-        self.proxyList = proxyList
-        self.timeout = 5
-        self.testUrl = "http://www.baidu.com/"
-
-    def checkProxy(self):
+    def checkProxy(self,pages=10):
         cookies = urllib2.HTTPCookieProcessor()
-        for proxy in self.proxyList:
+        proxy_list = self.crawl_proxy(pages)
+        checkedProxyList = []
+        for proxy in proxy_list:
             proxyHandler = urllib2.ProxyHandler({"http" : r'http://%s:%s' %(proxy[0],proxy[1])})
-            print proxy[0],proxy[1]
             opener = urllib2.build_opener(cookies,proxyHandler)
             opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:22.0) Gecko/20100101 Firefox/22.0')]
             t1 = time.time()
             try:
-                checkedProxyList = []
                 req = opener.open(self.testUrl, timeout=self.timeout)
                 result = req.read()
                 timeused = time.time() - t1
@@ -67,9 +61,14 @@ class ProxyCheck(threading.Thread):
                      continue
             except Exception,e:
                 continue
-        print checkedProxyList
         return checkedProxyList
 
-    def run(self):
-        self.checkProxy()
-
+    def pick_good_proxy(self,num=10):
+        proxy_list = self.checkProxy()
+        ranked_proxy = sorted(proxy_list,key=lambda x:x[2])
+        top_proxies = ranked_proxy[0:num]
+        proxyfw = open("proxy list.txt","w")
+        for proxy in top_proxies:
+            prox = str(proxy[0]) + " " + str(proxy[1]) + "\n"
+            proxyfw.write(prox)
+        proxyfw.close
